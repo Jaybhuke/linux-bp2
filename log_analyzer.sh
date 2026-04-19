6a372fdd7751850a1c4ec1d89327c75f0be4051b
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-19 19:36:13 +0000

    Checking for error count in each file using if loop

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -9,5 +9,14 @@ echo "---------------------------" >> $REPORT
 
 for file in $LOG_DIR/*.log
 do
+	echo "---------------" >> $REPORT
 	echo "Checking $file" >> $REPORT
+
+	ERROR_COUNT=$(grep -i "error" $file 2>/dev/null | wc -l)
+
+	if [ $ERROR_COUNT -gt 0 ]; then
+		echo "$file has $ERROR_COUNT errors" >> $REPORT
+	else
+		echo "No errors in $file" >> $REPORT
+	fi
 done
