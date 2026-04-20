dbd0c993eb2f0a3de4746682945e99f86ff425fc
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-20 19:36:25 +0000

    We have created temp files and sort errors by processing then store into main log file

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -2,21 +2,33 @@
 
 LOG_DIR="/var/log"
 REPORT="/home/ec2-user/learning/projects/project2/reports.log"
-
+TEMP_FILE="/home/ec2-user/learning/projects/project2/temp_reports.log"
 
 echo "Log analysis report $(date)" >> $REPORT
 echo "---------------------------" >> $REPORT
 
+
+> $TEMP_FILE
+
 for file in $LOG_DIR/*.log
 do
-	echo "---------------" >> $REPORT
-	echo "Checking $file" >> $REPORT
-
 	ERROR_COUNT=$(grep -i "error" $file 2>/dev/null | wc -l)
 
 	if [ $ERROR_COUNT -gt 0 ]; then
-		echo "$file has $ERROR_COUNT errors" >> $REPORT
-	else
-		echo "No errors in $file" >> $REPORT
+		echo "$ERROR_COUNT $file" >> $TEMP_FILE
 	fi
 done
+
+echo "Top Error Logs:" >> $REPORT
+
+if [ -s $TEMP_FILE ]; then
+	sort -nr $TEMP_FILE | while read count file
+	do
+		echo "$file has $count errors" >> $REPORT
+	done
+else
+	echo "No errors found in logs" >> $REPORT
+fi
+
+echo "" >> $REPORT
+echo "Analysis Complete" >> $REPORT
